5812cba8f55e378e57a5a50abbc4791bcb5d3a05
Andreas Monitzer <andreas@monitzer.com>
2018-05-14 02:56:39 +0200

    Implemented clock.

--- main/main.cpp
+++ main/main.cpp
@@ -1,46 +1,151 @@
 #include "esp_log.h"
 #include "fonts.h"
 #include "ssd1306.hpp"
-#include "driver/gpio.h"
-#include "driver/adc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <cstdio>
 #include <string>
-#include <sstream>
-#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "apps/sntp/sntp.h"
+
 using namespace std;
+
+#define TAG "Clock"
+const int CONNECTED_BIT = BIT0;
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+
+static void obtain_time(void);
+static void initialize_sntp(void);
+static void initialise_wifi(void);
+static esp_err_t event_handler(void *ctx, system_event_t *event);
 
 OLED oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 
 void myTask(void *pvParameters) {
-	uint16_t counter = 0;
-	ostringstream os;
+	time_t now;
+	struct tm timeinfo;
+	char strftime_buf[16];
+	oled.select_font(4);
 	while (1) {
-		os.str("");
+		time(&now);
+		localtime_r(&now, &timeinfo);
+		strftime(strftime_buf, sizeof(strftime_buf), "%H:%M", &timeinfo);
+
 		oled.clear();
-
-		os << counter;
-
-		oled.select_font(4);
-		oled.draw_string(0, 0, os.str(), WHITE, BLACK);
+		oled.draw_string(0, 5, strftime_buf, WHITE, BLACK);
 
 		oled.refresh(true);
-		vTaskDelay(100 / portTICK_PERIOD_MS);
-		counter++;
+		vTaskDelay(300 / portTICK_PERIOD_MS);
 	}
+}
+
+static void obtain_time(void)
+{
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    initialise_wifi();
+		ESP_LOGI(TAG, "Waiting for WiFi to be ready...");
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                        false, true, portMAX_DELAY);
+    initialize_sntp();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+    while(timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+
+    // ESP_ERROR_CHECK( esp_wifi_stop() );
+}
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, (char*)"pool.ntp.org");
+    sntp_init();
+}
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {};
+		strcpy((char*)&wifi_config.sta.ssid, CONFIG_WIFI_SSID);
+		strcpy((char*)&wifi_config.sta.password, CONFIG_WIFI_PASSWORD);
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+		ESP_LOGI(TAG, "System Event %d", event->event_id);
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 void app_main() {
-
 	if (oled.init()) {
 		ESP_LOGI("OLED", "oled inited");
-		oled.select_font(3);
+		oled.select_font(2);
 		oled.draw_string(0, 20, "Connecting...", WHITE, BLACK);
 		oled.refresh(true);
+
+		time_t now;
+		struct tm timeinfo;
+		time(&now);
+		localtime_r(&now, &timeinfo);
+		// Is time set? If not, tm_year will be (1970 - 1900).
+		if (timeinfo.tm_year < (2016 - 1900)) {
+				ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
+				obtain_time();
+				// update 'now' variable with current time
+				time(&now);
+		}
+		setenv("TZ", "TZ=CET-1CEST,M3.5.0/2,M10.5.0/3", 1);
+		tzset();
+
 		xTaskCreatePinnedToCore(&myTask, "clocktask", 2048, NULL, 5, NULL, 1);
 	} else {
 		ESP_LOGE("OLED", "oled init failed");
